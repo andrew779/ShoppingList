61e4a86c7edd14e0fda088e01af18090ca00ee27
wenzhongzheng <andrew198712@gmail.com>
2017-02-21 19:40:26 -0500

    base UI with storyboard

---
+++ ShoppingList/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  ShoppingList
+//
+//  Created by Wenzhong Zheng on 2017-02-21.
+//  Copyright © 2017 Wenzhong. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
